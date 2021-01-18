06abe8bbb85b4cbc0e2db38b039fc0e4c2d3fba0
vybhavpai <vybhavpai1999.vp@gmail.com>
2021-01-18 10:54:00 +0530

    testing kvm ppc 3

--- kvm-client/kvm_updated_ppc.c
+++ kvm-client/kvm_updated_ppc.c
@@ -234,12 +234,14 @@ ioctl() at run-time.
     printf("kvm regs gpr[1] = 0x%016lx\n", regs.gpr[1]);
     printf("kvm regs gpr[3] = 0x%016lx\n", regs.gpr[3]);
     printf("kvm regs pc = 0x%016lx\n", regs.pc);
+    string error_string = "kvm error has occurred, and exit is cause " ;
     /* Repeatedly run code and handle VM exits. */
     while (1)
     {
         ret = ioctl(vcpufd, KVM_RUN, NULL);
         if (ret == -1){
-            puts("kvm error has occurred, and exit is cause %x", run->exit_reason);
+            error_string = error_string +  run->exit_reason;
+            puts(error_string);
             err(1, "KVM_RUN");
         }
         switch (run->exit_reason)
